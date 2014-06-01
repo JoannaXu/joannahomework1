9e44039fe4a9bb74593a1f604c2759297230c3d8
Zhuo Xu <xuzhuo88@gmail.com>
2014-06-01 12:55:11 +0300

    my message

---
+++ math1-4.py
@@ -0,0 +1,25 @@
+def function():
+	passmath1 ()
+	print (1+1)
+	print (2+2)
+	print (3+3)
+
+def function():
+	passmath2 ()
+	print (1-1)
+	print (2-1)
+	print (3-1)
+
+def function():
+	passprint (1*1)
+	print (2*2)
+	print (3*3)
+
+def function():
+	passmath3()
+	print (1/1)
+	print (2/2)
+	print (3/3)
+
+	if __name__ == '__main__':
+		
